48a593d5a676daeb849e0cfefb674168172f985f
Jay Cribas <feverjay@gmail.com>
2017-09-19 14:53:47 -0700

    Writes ajax request to delete book from db

--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-
+  // GET
   getAllBooks()
 
   function getAllBooks(){
     $.get('https://mutably.herokuapp.com/books', responseData => {
       for(let book of responseData.books){
         $('div.list-group').append(`
-          <div class='book-box' id='${book.id}'>
+          <div class='book-box' id='${book._id}'>
             <div class='thumb'>
               <img class='img-thumbnail' src='${book.image}'></img>
             </div>
@@ -14,6 +14,7 @@ $(document).ready(function(){
               <h3>${book.title}</h3>
               <p>${book.author}</p>
               <p>${book.releaseDate}</p>
+              <button class='delete-book'>Delete</button>
             </div>
           </div>
           `)
@@ -21,10 +22,25 @@ $(document).ready(function(){
     })
   }
 
+  // POST
   $('#new-book').on('submit', event => {
     event.preventDefault()
     $.post('https://mutably.herokuapp.com/books', $('#new-book').serialize())
   })
+
+  // DELETE
+  $(document).on('click', '.delete-book', event => {
+    let id = $(event.target).closest('div.book-box').attr('id')
+    $.ajax({
+      url: `https://mutably.herokuapp.com/books/${id}`,
+      type: 'DELETE',
+      success: function(result){
+        console.log('Deleted the book! ', result)
+      }
+    })
+  })
+
+
 
   $('#add-book').on('click', event => {
     $('.form-modal').css('display', 'block')
